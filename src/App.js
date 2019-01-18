bcbf3ac7bd431e336d3db47f52c26d8aac44d185
FarrandTom <33008723+FarrandTom@users.noreply.github.com>
2019-01-18 09:13:19 +0000

    Finishing dynamic lists etc.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
   state = {
     persons: [
-      {name: "Tom", age: 24},
-      {name: "Cazza", age: 56},
-      {name: "Richy", age: 57}
+      {id: 'rihs4iaiowj3', name: "Tom", age: 24},
+      {id: 'vaoiwjd109u1', name: "Cazza", age: 56},
+      {id: '2soijsoijefo', name: "Richy", age: 57}
     ],
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    // console.log('WAGWAN');
-    // DON'T DO THIS this.state.persons[0].name = 'Thomas';
-    this.setState( {
-      persons: [
-      {name: newName, age: 24},
-      {name: "Carole", age: 56},
-      {name: "Richy", age: 57}
-    ]
-  })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    // Remember not to manipulate the state directly. javascript objects are 
+    // reference types therefore you get a pointer back which is mutable. 
+    // Use the spread operator again.
+    const person = {...this.state.persons[personIndex]};
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons})
   }
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        {name: "Tom", age: 24},
-        {name: event.target.value, age: 56},
-        {name: "Richy", age: 57}
-      ]
-    })
+  deletePersonHandler = (personIndex) => {
+    // Arrays and objects are reference types.
+    // So need to call the spread operator (could alternately use the slice method),
+    // to get a copy of the persons array- rather than a pointer to the
+    // original array. Always update state in an immutable way.
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
   }
 
   togglePersonsHandler = () => {
@@ -53,10 +58,13 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person, index) => {
             return <Person 
+                click={() => this.deletePersonHandler(index)}
                 name={person.name}
-                age={person.age}/>
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       );
